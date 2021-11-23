387e8d79c3b4e5fd418401fba327a2c179f66705
AlessandroVano <alessandrovanoli.16@gmail.com>
2021-11-23 16:09:33 +0100

    preparazione file di lavoro + prima bozza header

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,40 @@
 <template>
   <div id="app">
-   
+   <!-- HEADER -->
+   <header>
+     <Header />
+   </header>
+   <!-- MAIN -->
+   <main></main>
+
+   <!-- FOOTER -->
+   <footer></footer>
+       
+
+
+
   </div>
 </template>
 
 <script>
+import Header from '@/components/Header.vue'
 
 
 export default {
   name: 'App',
   components: {
- 
-  }
+       Header,
+  },
 }
+
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import '@/style/globals';
+@import '@/style/utilities';
+
+  
+
+  
+  
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+<header class="container">
+    <a href="/">
+    <img src="../assets/dc-logo.png" alt="dc-logo.png"></a>
+    <nav>
+        <ul>
+            <li>
+                <a href="/characters">CHARACTERS</a>
+            </li>
+             <li>
+                <a href="/comics">COMICS</a>
+            </li>
+             <li>
+                <a href="/movies">MOVIES</a>
+            </li>
+             <li>
+                <a href="/tv">TV</a>
+            </li>
+             <li>
+                <a href="/games">GAMES</a>
+            </li>
+             <li>
+                <a href="/collectibles">COLLECTIBLES</a>
+            </li>
+             <li>
+                <a href="/videos">VIDEOS</a>
+            </li>
+             <li>
+                <a href="/fans">FANS</a>
+            </li>
+             <li>
+                <a href="/news">NEWS</a>
+            </li>
+             <li>
+                <a href="/shop">SHOP</a>
+            </li>
+        </ul>
+    </nav>
+</header>
+</template>
+
+<script>
+export default {
+name: 'Header',
+}
+</script>
+
+<style>
+
+</style>
